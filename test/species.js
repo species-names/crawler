93178ce64d5a2bf9c5048d8b29d644ed7bb5cbf5
Ivo Bathke <ivo.bathke@gmail.com>
2016-11-07 11:17:15 +0100

    species crawling

--- test/species.js
+++ test/species.js
@@ -1,6 +1,10 @@
 'use strict';
 var assert = require('assert');
 var species = require('../lib/crawler/species');
+var accessor = require('../lib/crawler/accessor');
+var parsenames = require('../lib/crawler/parsenames');
+
+var fixture = require('./fixtures/species.json');
 
 describe('species', function () {
   it('should detect species', function () {
@@ -14,5 +18,13 @@ describe('species', function () {
   it('should not detect other species', function () {
     const isSpecie = species.detectSpecies('Genera', 'Tada influxis');
     assert.equal(false, isSpecie);
+  });
+  it('should create object', function () {
+    const text = accessor.getContentFromQuery(fixture);
+    const names = parsenames(text);
+    const generaObject = species.createSpeciesObject('Muscicapa caerulescens', names);
+    assert.equal('Muscicapa caerulescens', generaObject.scientific_name);
+    assert.equal('en', generaObject.common_names[1].lang);
+    assert.equal('Askflugsnappare', generaObject.common_names[4].name);
   });
 });
