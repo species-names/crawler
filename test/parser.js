53a79798e32241baf1fde981b97ef9414bb72b8c
Ivo Bathke <ivo.bathke@gmail.com>
2016-11-23 09:50:45 +0100

    square bracket links

--- test/parser.js
+++ test/parser.js
@@ -5,6 +5,7 @@ var parser = require('../lib/crawler/parser');
 
 var fixtureFamilia = require('./fixtures/familia_muscicapidae_query.json');
 var fixtureFamiliaGenus = require('./fixtures/familia_with_genus.json');
+var fixtureFamiliaeLinkFormat2 = require('./fixtures/familiae_linkformat2.json');
 
 describe('parser', function () {
   it('should parse links from genera', function () {
@@ -16,5 +17,10 @@ describe('parser', function () {
     const links = parser.parseLinksFromQueryData('Genus', fixtureFamiliaGenus);
     assert.equal('Sagittarius', links[0]);
     assert.equal(1, links.length);
+  });
+  it('should parse links from familia with square bracket link format', function () {
+    const links = parser.parseLinksFromQueryData('Familiae', fixtureFamiliaeLinkFormat2);
+    assert.equal('Dromaiidae', links[1]);
+    assert.equal(2, links.length);
   });
 });
