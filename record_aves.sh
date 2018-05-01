f33b65923c7b7aa20a999e15b29167be9c168845
Ivo Bathke <ivo.bathke@gmail.com>
2018-05-01 18:38:35 +0200

    record aves script

--- record_aves.sh
+++ record_aves.sh
@@ -1,109 +1,116 @@
-bin/species-names-crawler -o Accipitriformes
-bin/species-names-crawler -o Anseriformes
-bin/species-names-crawler -o Apodiformes
-bin/species-names-crawler -o Apterygiformes
-Caprimulgiformes
-Casuariiformes
-Charadriiformes
-Ciconiiformes
-Coliiformes
-Columbiformes
-Coraciiformes
-Cuculiformes
-Falconiformes
-Galliformes
-Gaviiformes
-Gruiformes
-Musophagiformes
-Pelecaniformes
-Phoenicopteriformes
-Piciformes
-Podicipediformes
-Procellariiformes
-Psittaciformes
-Pteroclidiformes
-Rheiformes
-Sphenisciformes
-Strigiformes
-Struthioniformes
-Tinamiformes
-Trochiliformes
-Trogoniformes
+#!/bin/bash
 
-problems with Passeriformes, try with Familia:
-bin/species-names-crawler -f Acanthisittidae
-Pipridae
-Cotingidae
-Oxyruncidae
-Onychorhynchidae
-Tityridae
-Pipritidae
-Platyrinchidae
-Tachurisidae
-Rhynchocyclidae
-Tyrannidae
-Thamnophilidae
-Furnariidae
-Dendrocolaptidae
-Pittidae
-Philepittida
-Eurylaimidae
-Aegithinidae
-Artamidae
-Callaeidae
-Campephagidae
-Chaetopidae
-Chloropseidae
-Cinclosomatidae
-Corcoracidae
-Corvidae
-Dicruridae
-Eupetidae
-Grallinidae
-Irenidae
-Laniidae
-Malaconotidae
-Monarchidae
-Mohouidae
-Notiomystidae
-Oriolidae
-Orthonychidae
-Pachycephalidae
-Paradisaeidae
-Petroicidae
-Picathartidae
-Pityriaseidae
-Platysteiridae
-Pomatostomidae
-Prionopidae
-Rhipiduridae
-Vangidae
-Vireonidae
+declare -a arrOrdines=(
+                "Accipitriformes"
+                "Anseriformes"
+                "Apodiformes"
+                "Apterygiformes"
+                "Caprimulgiformes"
+                "Casuariiformes"
+                "Charadriiformes"
+                "Ciconiiformes"
+                "Coliiformes"
+                "Columbiformes"
+                "Coraciiformes"
+                "Cuculiformes"
+                "Falconiformes"
+                "Galliformes"
+                "Gaviiformes"
+                "Gruiformes"
+                "Musophagiformes"
+                "Passeroidea"
+                "Pelecaniformes"
+                "Phoenicopteriformes"
+                "Piciformes"
+                "Podicipediformes"
+                "Procellariiformes"
+                "Psittaciformes"
+                "Pteroclidiformes"
+                "Rheiformes"
+                "Sphenisciformes"
+                "Strigiformes"
+                "Struthioniformes"
+                "Sylvioidea"
+                "Tinamiformes"
+                "Trochiliformes"
+                "Trogoniformes"
+                )
 
-Paridae
-Regulidae
-Remizidae
-Stenostiridae
-Certhiidae
-Polioptilidae
-Sittidae
-Tichodromidae
-Troglodytidae
-Bombycillidae
-Buphagidae
-Cinclidae
-Mimidae
-Muscicapidae
-Sturnidae
-Turdidae
+for i in "${arrOrdines[@]}"
+do
+  bin/species-names-crawler -c Aves -o $i
+done
 
-Ordo:
-Passeroidea
-Sylvioidea
-
-Genus:
-Hyliota
-
-problems with Turniciformes
-problems with Eonessa
-problems with Manu
+# problems with Passeriformes, try with Familia:
+# bin/species-names-crawler -f Acanthisittidae
+# Pipridae
+# Cotingidae
+# Oxyruncidae
+# Onychorhynchidae
+# Tityridae
+# Pipritidae
+# Platyrinchidae
+# Tachurisidae
+# Rhynchocyclidae
+# Tyrannidae
+# Thamnophilidae
+# Furnariidae
+# Dendrocolaptidae
+# Pittidae
+# Philepittida
+# Eurylaimidae
+# Aegithinidae
+# Artamidae
+# Callaeidae
+# Campephagidae
+# Chaetopidae
+# Chloropseidae
+# Cinclosomatidae
+# Corcoracidae
+# Corvidae
+# Dicruridae
+# Eupetidae
+# Grallinidae
+# Irenidae
+# Laniidae
+# Malaconotidae
+# Monarchidae
+# Mohouidae
+# Notiomystidae
+# Oriolidae
+# Orthonychidae
+# Pachycephalidae
+# Paradisaeidae
+# Petroicidae
+# Picathartidae
+# Pityriaseidae
+# Platysteiridae
+# Pomatostomidae
+# Prionopidae
+# Rhipiduridae
+# Vangidae
+# Vireonidae
+#
+# Paridae
+# Regulidae
+# Remizidae
+# Stenostiridae
+# Certhiidae
+# Polioptilidae
+# Sittidae
+# Tichodromidae
+# Troglodytidae
+# Bombycillidae
+# Buphagidae
+# Cinclidae
+# Mimidae
+# Muscicapidae
+# Sturnidae
+# Turdidae
+#
+# Genus:
+# Hyliota
+#
+# problems with Turniciformes
+# problems with Eonessa
+# problems with Manu
