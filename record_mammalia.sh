8f80c3386224a160727e562887ecb349c000613e
Ivo Bathke <ivo.bathke@gmail.com>
2018-06-24 10:27:53 +0200

    record mammalia familiae

--- record_mammalia.sh
+++ record_mammalia.sh
@@ -1,53 +1,84 @@
-Familiae:
-Cynodontia
-Canidae
-Nandiniidae
-Prionodontidae
-Felidae
-Herpestidae
-Eupleridae
-Hyaenidae
-Viverridae
-Balaenidae
-Balaenopteridae
-Eschrichtiidae
-Neobalaenidae
-Delphinidae
-Iniidae
-Kogiidae
-Lipotidae
-Monodontidae
-Phocoenidae
-Physeteridae
-Platanistidae
-Pontoporiidae
-Ziphiidae
-Pteropodidae (findet nix)
+#!/bin/bash
+declare -a arr=(
+  "Cynodontia"
+  "Canidae"
+  "Nandiniidae"
+  "Prionodontidae"
+  "Felidae"
+  "Herpestidae"
+  "Eupleridae"
+  "Hyaenidae"
+  "Viverridae"
+  "Balaenidae"
+  "Balaenopteridae"
+  "Eschrichtiidae"
+  "Neobalaenidae"
+  "Delphinidae"
+  "Iniidae"
+  "Kogiidae"
+  "Lipotidae"
+  "Monodontidae"
+  "Phocoenidae"
+  "Physeteridae"
+  "Platanistidae"
+  "Pontoporiidae"
+  "Ziphiidae"
+)
 
-bin/species-names-crawler -c Mammalia -o Cingulata
-Chiroptera
-Afrosoricida
-Artiodactyla
-Dasyuromorphia
-Dermoptera
-Didelphimorphia
-Diprotodontia
-Erinaceomorpha
-Hyracoidea
-Lagomorpha
-Macroscelidea (problem)
-Microbiotheria
-Monotremata
-Notoryctemorphia
-Paucituberculata
-Peramelemorphia
-Perissodactyla
-Pholidota
-Pilosa
-Primates (problem)
-Proboscidea
-Rodentia
-Scandentia
-Sirenia
-Soricomorpha
-Tubulidentata
+for i in "${arr[@]}"
+do
+  bin/species-names-crawler -c Mammalia -f $i
+done
+# Familiae:
+# Cynodontia
+# Canidae
+# Nandiniidae
+# Prionodontidae
+# Felidae
+# Herpestidae
+# Eupleridae
+# Hyaenidae
+# Viverridae
+# Balaenidae
+# Balaenopteridae
+# Eschrichtiidae
+# Neobalaenidae
+# Delphinidae
+# Iniidae
+# Kogiidae
+# Lipotidae
+# Monodontidae
+# Phocoenidae
+# Physeteridae
+# Platanistidae
+# Pontoporiidae
+# Ziphiidae
+# Pteropodidae (findet nix)
+#
+# bin/species-names-crawler -c Mammalia -o Cingulata
+# Chiroptera
+# Afrosoricida
+# Artiodactyla
+# Dasyuromorphia
+# Dermoptera
+# Didelphimorphia
+# Diprotodontia
+# Erinaceomorpha
+# Hyracoidea
+# Lagomorpha
+# Macroscelidea (problem)
+# Microbiotheria
+# Monotremata
+# Notoryctemorphia
+# Paucituberculata
+# Peramelemorphia
+# Perissodactyla
+# Pholidota
+# Pilosa
+# Primates (problem)
+# Proboscidea
+# Rodentia
+# Scandentia
+# Sirenia
+# Soricomorpha
+# Tubulidentata
